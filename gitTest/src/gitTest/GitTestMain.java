0a739316ed3268c70a666a98059b165aad03e26e
nijesung2 <nijesung@gmail.com>
2021-08-24 15:46:42 +0900

    test!

--- gitTest/src/gitTest/GitTestMain.java
+++ gitTest/src/gitTest/GitTestMain.java
@@ -5,7 +5,7 @@ public class GitTestMain {
 	public static void main(String[] args) {
 		
 		for(int i=1; i < 5; i++) {
-			System.out.println("vim 극혐");
+			System.out.println("vim 어렵다");
 		}
 
 	}
